c499e1013a59ff2cefebaed0211ed202b8e0ad04
Jerry Chen <jerry.chen33193@gmail.com>
2022-10-04 00:08:53 -0400

    alternating colors JS

--- connectFour.js
+++ connectFour.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+// variables for gameplay go here
+
+const changeStatus = document.querySelector('#board')
+let turn = 'yellow'
+let replayGame = document.getElementById('replay')
+let value = ['', '', '', '', '', '', '', '', '']
+const winner = document.querySelector('#valueWinner')
+let noWinner = 'draw'
+let gameLive = true
+
+// alternate from yellow to blue
+
+document.querySelectorAll('.box').forEach((box) => {
+  box.addEventListener('click', () => {
+    if (turn === 'yellow') {
+      turn = 'blue'
+      return (box.style.backgroundColor = 'yellow')
+    } else {
+      turn = 'yellow'
+      return (box.style.backgroundColor = 'blue')
+    }
+  })
+})
