143dad985e93da392eafa09694ae5602c0e3e060
Jerry Chen <jerry.chen33193@gmail.com>
2022-10-04 11:17:02 -0400

    alternate players(color)

--- connectFour.js
+++ connectFour.js
@@ -1,23 +1,44 @@
 // variables for gameplay go here
-
 const changeStatus = document.querySelector('#board')
-let turn = 'yellow'
-let replayGame = document.getElementById('replay')
+let inTurn = 'yellow'
+// let replayGame = document.getElementById('replay')
 let value = ['', '', '', '', '', '', '', '', '']
 const winner = document.querySelector('#valueWinner')
 let noWinner = 'draw'
 let gameLive = true
+const displayInTurn = document.querySelector('#inTurnPlayer')
+// let resetGame = () => {
+//   for (let i = 0; i <= 43; i++) {
+//     clearBox(i)
+//   }
+// }
 
 // alternate from yellow to blue
 
 document.querySelectorAll('.box').forEach((box) => {
   box.addEventListener('click', () => {
-    if (turn === 'yellow') {
-      turn = 'blue'
+    // if (box[i + 6].classList.contains('taken')) {
+    if (inTurn === 'yellow') {
+      // box[i].classList.add('taken')
+      // box[i].classList.add('yellow')
+      inTurn = 'blue'
+      displayInTurn.innerHTML = inTurn
       return (box.style.backgroundColor = 'yellow')
     } else {
-      turn = 'yellow'
+      inTurn = 'yellow'
       return (box.style.backgroundColor = 'blue')
     }
   })
 })
+
+// const replayGame = document.getElementById('replay')
+
+// replayGame.addEventListener('click', () => {
+//   resetGame()
+// })
+// resetGame()
+// document.querySelector('.replay').addEventListener('click', () => {
+//   document.querySelectorAll('.box').forEach((box) => {
+//     box.remove()
+//   })
+// })
